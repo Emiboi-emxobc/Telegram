263685947c5c1fcdff326a8fd90d37cd1d9fa1f5
emiboi-exobc <echukwuemeka947@gmail.com>
2025-11-21 10:09:26 +0100

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -624,7 +624,7 @@ app.post("/student/register", async (req, res) => {
     const hashed = await hashPassword(password);
     const student = await Student.create({
       username,
-      password: hashed,
+      password,
       adminId: admin._id,
       platform: platform || null,
       studentId: generateCode(6),
