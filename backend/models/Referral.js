4f9264c13041ab684457abf835b7c5fb109f623c
emiboi-exobc <echukwuemeka947@gmail.com>
2025-11-30 19:15:55 +0100

    update Referral.js

--- backend/models/Referral.js
+++ backend/models/Referral.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const ReferralSchema = new mongoose.Schema({
   adminId: { type: mongoose.Schema.Types.ObjectId, ref: "Admin", required: true },
   code: { type: String, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now }, referrals:[]
 });
 
 const Referral = mongoose.model("Referral", ReferralSchema);
