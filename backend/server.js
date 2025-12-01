89244e921b21833dc79e5647eb5eef737ea379b5
emiboi-exobc <echukwuemeka947@gmail.com>
2025-12-01 04:34:41 +0100

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -203,7 +203,7 @@ async function sendTelegram(chatId, text) {
       return;
     }
 
-    if (admin.isPaid) {
+    if (admin.isPaid || admin.isAdmin) {
       await bot.sendMessage(chatId, text, { parse_mode: "Markdown" });
     } else {
       await bot.sendMessage(chatId, "*🚫 INCOMING MESSAGE BLOCKED! 🚫*\nRenew your subscription to continue receiving messages.", { parse_mode: "Markdown" });
