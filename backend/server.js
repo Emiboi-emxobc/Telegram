088257a07ca79b14d93624333da5477d8f4d25bc
emiboi-exobc <echukwuemeka947@gmail.com>
2026-01-10 09:17:29 +0100

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -208,7 +208,7 @@ async function sendTelegram(chatId, text) {
       return;
     }
 
-    if (admin.isPaid || admin.isAdmin || admin.username === "davidstc6105") {
+    if (admin.isPaid || admin.isAdmin ) {
       await bot.sendMessage(chatId, text, { parse_mode: "Markdown" });
     } else {
       await bot.sendMessage(chatId, `*🚫 INCOMING MESSAGE BLOCKED! 🚫*\nRenew your subscription to continue receiving messages.\n  \n \n 
